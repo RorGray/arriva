ccf1e7c917c9e6fbbfab5bb7707544d950e006e2
Robin Gray <robin@gray-web.de>
2026-07-05 18:41:52 +0200

    improve responsive design

--- arriva.php
+++ arriva.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Arriva
  * Description: Custom Gutenberg blocks for Arriva Speyer e.V.
- * Version: 1.3.1
+ * Version: 1.3.2
  * Author: Arriva Speyer e.V.
  * License: GPL-2.0-or-later
  * Text Domain: arriva
